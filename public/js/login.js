370283bca1e9938a19a57ee6ebc5a9fec9d767ba
Matheus da Cruz <matheuswachcruz@gmail.com>
2020-10-27 16:55:10 -0300

    Atualizando

--- public/js/login.js
+++ public/js/login.js
@@ -38,3 +38,22 @@ function verificarSeEhVazio() {
   })
   
 }
+
+// Fechar o modal
+function fecharModal() {
+  document.querySelector('.try-again-button').addEventListener("click", () => {
+    const modalOverlay = document.querySelector('.modal-overlay')
+    modalOverlay.classList.remove("active")
+  })
+}
+
+//Ocultar senha
+function ocultarSenha() {
+  const senha = document.getElementById("senha") //pegando o input da senha
+  if(senha.type === "password") { // caso o input seja do tipo "password", irá trocar para o tipo "text", mostrando, assim, a senha
+    senha.type = "text"
+  } else {
+    senha.type = "password" // se for do tipo "text", irá cobrir a senha
+  }
+
+}
